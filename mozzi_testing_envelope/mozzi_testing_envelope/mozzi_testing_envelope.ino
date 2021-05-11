1025c1c4543ce125c716691157c318fcdc980059
szivarvanykutya <szivarvanykutya@gmail.com>
2021-05-11 06:46:10 +0200

    envelope

--- mozzi_testing_envelope/mozzi_testing_envelope/mozzi_testing_envelope.ino
+++ mozzi_testing_envelope/mozzi_testing_envelope/mozzi_testing_envelope.ino
@@ -1,9 +1,41 @@
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
+#include <Ead.h>
+#include <EventDelay.h>
+#include <mozzi_rand.h>
+
+
+#define CONTROL_RATE 256
+
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+EventDelay kDelay;
+Ead kEnvelope(CONTROL_RATE);
+int gain;
 void setup() {
   // put your setup code here, to run once:
-
+  aSin.setFreq(240);
+  startMozzi(CONTROL_RATE);
+  kDelay.start(1000);
 }
 
+void updateControl(){
+if (kDelay.ready()) {
+unsigned int duration = 1800;
+unsigned int attack = 10;
+unsigned int decay = duration - attack;
+kEnvelope.start(attack,decay);
+kDelay.start(duration+500);
+}
+gain = (int) kEnvelope.next();
+}
+  
+int updateAudio(){
+return (gain*aSin.next()) >> 8; 
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  audioHook();
 
 }
